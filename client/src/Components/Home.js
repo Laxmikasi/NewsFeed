c2cc8fe68194bac4437de8b5eb49bf5bc1667da6
saivanithak <98114410+saivanithak@users.noreply.github.com>
2024-01-22 14:50:31 +0530

    new

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React,{useState,useEffect} from 'react'
 import Post from './Post';
 import Navbar from './Navbar';
 import axios from 'axios';
 import { AiFillLike } from 'react-icons/ai';
-import { AiFillDislike } from 'react-icons/ai';
+import { AiFillDislike } from "react-icons/ai";
 import { FaCommentAlt } from 'react-icons/fa';
 import { IoMdShare } from 'react-icons/io';
-import { ToastContainer, toast } from 'react-toastify';
 
 
 const Home = () => {
   const [allPosts, setAllPosts] = useState([]);
-  const [commentVisible, setCommentVisible] = useState(false);
+  const [commentVisible, setCommentVisible] = useState(null);
   const [moreVisible, setMoreVisible] = useState(false);
   const [comment, setComment] = useState('');
 
-  const handleCommentClick = () => {
-    setCommentVisible(!commentVisible);
+  const handleCommentClick = (postId) => {
+    setCommentVisible(commentVisible === postId ? null : postId);
   };
 
   const handleCommentChange = (e) => {
@@ -27,125 +26,8 @@ const Home = () => {
     setMoreVisible(!moreVisible);
   };
 
-  const fetchPosts = async () => {
-    try {
-      const response = await axios.get("http://localhost:5000/api/allPosts");
-      const responseData = response.data.reverse();
-      console.log(responseData);
-      setAllPosts(responseData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  // const handleLike = (e,postId, userId) => {
-
-  //   e.preventDefault();
-    
-  //   const token = localStorage.getItem("token");
-  
-  //   if (!token) {
-  //     toast.error("Please login to add items to the wishlist.");
-  //     window.location.href = "/login";
-  //     return;
-  //   }
-  //   axios.post(`http://localhost:5000/api/like/${postId}`)
-  //     .then(response => {
-  //       console.log(response.data);
-  //       setAllPosts(prevPosts => {
-  //         return prevPosts.map(post => (post._id === postId ? response.data : post));
-  //       });
-  //     })
-  //     .catch(error => console.error('Error liking media:', error));
-  // };
-  
-  // const handleDislike = (e,postId, userId) => {
-
-  //   e.preventDefault();
-  //   const token = localStorage.getItem("token");
-  
-  //   if (!token) {
-  //     toast.error("Please login to add items to the wishlist.");
-  //     window.location.href = "/login";
-  //     return;
-  //   }
-  //   axios.post(`http://localhost:5000/api/dislike/${postId}`)
-  //     .then(response => {
-  //       console.log(response.data);
-  //       setAllPosts(prevPosts => {
-  //         return prevPosts.map(post => (post._id === postId ? response.data : post));
-  //       });
-  //     })
-  //     .catch(error => console.error('Error disliking media:', error));
-  // };
-
-  // const handleLike = (e,postId, userId) => {
-
-  //   e.preventDefault();
-    
-  //   const token = localStorage.getItem("token");
-  
-  //   if (!token) {
-  //     toast.error("Please login to add items to the wishlist.");
-  //     window.location.href = "/login";
-  //     return;
-  //   }
-  //   axios.post(`http://localhost:5000/api/like/${postId}`)
-  //     .then(response => {
-  //       console.log(response.data);
-  //       setAllPosts(prevPosts => {
-  //         return prevPosts.map(post => (post._id === postId ? response.data : post));
-  //       });
-  //     })
-  //     .catch(error => console.error('Error liking media:', error));
-  // };
-  
-  // const handleDislike = (e,postId, userId) => {
-
-  //   e.preventDefault();
-  //   const token = localStorage.getItem("token");
-  
-  //   if (!token) {
-  //     toast.error("Please login to add items to the wishlist.");
-  //     window.location.href = "/login";
-  //     return;
-  //   }
-  //   axios.post(`http://localhost:5000/api/dislike/${postId}`)
-  //     .then(response => {
-  //       console.log(response.data);
-  //       setAllPosts(prevPosts => {
-  //         return prevPosts.map(post => (post._id === postId ? response.data : post));
-  //       });
-  //     })
-  //     .catch(error => console.error('Error disliking media:', error));
-  // };
-  
-
-  const handleLike = (e, postId, userId) => {
-    e.preventDefault();
-  
-    const token = localStorage.getItem("token");
-  
-    if (!token) {
-      toast.error("Please login to add items to the wishlist.");
-      window.location.href = "/login";
-      return;
-    }
-  
-    axios.post(
-      `http://localhost:5000/api/like/${postId}`,
-      null,  // No request data needed
-      {
-        headers: {
-          'x-token': token,
-          
-        },
-      }
-    )
+  const handleLike = (postId, userId) => {
+    axios.post(`http://localhost:5000/api/like/${postId}/${userId}`)
       .then(response => {
         console.log(response.data);
         setAllPosts(prevPosts => {
@@ -154,28 +36,9 @@ const Home = () => {
       })
       .catch(error => console.error('Error liking media:', error));
   };
-  
-  const handleDislike = (e, postId, userId) => {
-    e.preventDefault();
-  
-    const token = localStorage.getItem("token");
-  
-    if (!token) {
-      toast.error("Please login to add items to the wishlist.");
-      window.location.href = "/login";
-      return;
-    }
-  
-    axios.post(
-      `http://localhost:5000/api/dislike/${postId}`,
-      null,  // No request data needed
-      {
-        headers: {
-          'x-token': token,
-          
-        },
-      }
-    )
+
+  const handleDislike = (postId, userId) => {
+    axios.post(`http://localhost:5000/api/dislike/${postId}/${userId}`)
       .then(response => {
         console.log(response.data);
         setAllPosts(prevPosts => {
@@ -184,20 +47,28 @@ const Home = () => {
       })
       .catch(error => console.error('Error disliking media:', error));
   };
-  
 
+  const handleCommentSubmit = async (postId) => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/comments', {
+        userId: 'your_user_id', // Replace with the actual user ID
+        postId,
+        text: comment,
+      });
 
+      console.log('Comment submitted:', response.data);
+      setComment('');
 
-
-
-
-
+      // You might want to update your state with the new comment data here
+    } catch (error) {
+      console.error('Error submitting comment:', error);
+    }
+  };
 
   useEffect(() => {
-    // Fetch vitals data from the API
     const fetchPosts = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/allPosts");
+        const response = await axios.get('http://localhost:5000/api/allPosts');
         const responseData = response.data.reverse();
         console.log(responseData);
         setAllPosts(responseData);
@@ -208,104 +79,98 @@ const Home = () => {
 
     fetchPosts();
   }, []);
-      
-
-
-
-
 
   return (
-<>
-<Navbar/>
-    <div className="App">
-      <div style={{width:'75%',margin:'auto'}}>
-
-      
-      <header className="App-header">
-        <h1>Facebook Feed</h1>
-      </header>
-      <div className="feed-container">
-        {allPosts.map((post) => (
-          
-          <div className="post" key={post.id}>
-          <div className="post-div">
-            <img className="post-profile-pic" src={''} alt="img" />
-            <h2 style={{ margin: '0%', marginLeft: '15px' }}>{post.Author.Name}</h2>
-          </div>
-          {/* <p>{text}</p> */}
-          <div className="post-div1">
-           
-            <img className="post-picture" 
-            src={`http://localhost:5000${post.image}`}
-             alt="img" />
-            {/* <div className='post-div5'>
-            <p>{text}</p>
-            {moreVisible &&(<p>{text}</p>)}
-                <button className='post-button' onClick={handleMoreclick}>Show More</button>
-            </div> */}
+    <>
+      <Navbar />
+      <div className="App">
+        <div style={{ width: '75%', margin: 'auto' }}>
+          <header className="App-header">
+            <h1>Facebook Feed</h1>
+          </header>
+          <div className="feed-container">
+            {allPosts.map((post) => (
+              <div className="post" key={post._id}>
+                <div className="post-div">
+                  <img className="post-profile-pic" src={''} alt="img" />
+                  <h2 style={{ margin: '0%', marginLeft: '15px' }}>{post.Author.Name}</h2>
                 </div>
-                <div >
-                <h3 style={{padding:'10px',margin:'0%'}}>{post.title}</h3>
-                {moreVisible && (
-                    <div className='post-div5'>
-                    <p className='post-p'>{post.content}</p>
-                    <button className='post-button'  onClick={handleMoreclick}>Show Less</button>
-                    </div>
-                )}
-                {!moreVisible && (
-                    <div className='post-div5'>
-                    <p>{post.content.substring(0, 200)}</p>
-                    <button className='post-button' onClick={handleMoreclick}>Show More</button>
-                    </div>
-                )}
+                <div className="post-div1">
+                  <img
+                    className="post-picture"
+                    src={`http://localhost:5000${post.image}`}
+                    alt="img"
+                  />
                 </div>
-          <div className="post-div4">
-            <div className="post-div3"  onClick={(e) => handleLike(e,post._id,post.Author.UserId)}>
-              <AiFillLike className="post-like" />
-              <p style={{ margin: '0%', marginLeft: '5px' }}>{post.likes} Likes</p>
-            </div>
-    
-            <div className="post-div3"  onClick={(e) => handleDislike(e,post._id,post.Author.UserId)}>
-              <AiFillDislike className="post-like" />
-              <p style={{ margin: '0%', marginLeft: '5px' }}>{post.dislikes} Dislikes</p>
-            </div>
-    
-            <div className="post-div3" onClick={handleCommentClick}>
-              <FaCommentAlt className="post-like" />
-              <p style={{ margin: '0%', marginLeft: '5px' }}>Comment</p>
-            </div>
-    
-            <div className="post-div3">
-              <IoMdShare className="post-like" />
-              <p style={{ margin: '0%', marginLeft: '5px' }}>Share</p>
-            </div>
-          </div>
-          {commentVisible && (
-  <div className="comment-section1">
-    <input
-      type="text"
-      placeholder="Write a comment..."
-      value={comment}
-      onChange={handleCommentChange}
-    />
-    <button onClick={() => handleCommentPost(post._id)}>Post </button>
-  </div>
-)}
+                <div>
+                  <h3 style={{ padding: '10px', margin: '0%' }}>{post.title}</h3>
+                  {moreVisible && (
+                    <div className="post-div5">
+                      <p className="post-p">{post.content}</p>
+                      <button className="post-button" onClick={handleMoreclick}>
+                        Show Less
+                      </button>
+                    </div>
+                  )}
+                  {!moreVisible && (
+                    <div className="post-div5">
+                      <p>{post.content.substring(0, 200)}</p>
+                      <button className="post-button" onClick={handleMoreclick}>
+                        Show More
+                      </button>
+                    </div>
+                  )}
+                </div>
+                <div className="post-div4">
+                  <div
+                    className="post-div3"
+                    onClick={() => handleLike(post._id, post.Author.UserId)}
+                  >
+                    <AiFillLike className="post-like" />
+                    <p style={{ margin: '0%', marginLeft: '5px' }}>{post.likes} Likes</p>
+                  </div>
 
+                  <div
+                    className="post-div3"
+                    onClick={() => handleDislike(post._id, post.Author.UserId)}
+                  >
+                    <AiFillDislike className="post-like" />
+                    <p style={{ margin: '0%', marginLeft: '5px' }}>{post.dislikes} Dislikes</p>
+                  </div>
+
+                  <div
+                    className="post-div3"
+                    onClick={() => handleCommentClick(post._id)}
+                  >
+                    <FaCommentAlt className="post-like" />
+                    <p style={{ margin: '0%', marginLeft: '5px' }}>Comment</p>
+                  </div>
+
+                  <div className="post-div3">
+                    <IoMdShare className="post-like" />
+                    <p style={{ margin: '0%', marginLeft: '5px' }}>Share</p>
+                  </div>
+                </div>
+                {commentVisible === post._id && (
+                  <div className="comment-section1">
+                    <input
+                      type="text"
+                      placeholder="Write a comment..."
+                      value={comment}
+                      onChange={handleCommentChange}
+                    />
+                    <button type="submit" onClick={() => handleCommentSubmit(post._id)}>
+                      Submit
+                    </button>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-
-
-
-
-        ))}
       </div>
-      <ToastContainer  />
-    </div>
-    </div>
     </>
   );
-    
-  
-}
+};
 
-export default Home
+export default Home;
